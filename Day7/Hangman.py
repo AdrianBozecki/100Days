475d751033db3dc3c13b9cdeee0ef13a7ba80ef3
Adrian Bożęcki <mail.bozecki@gmail.com>
2022-01-18 00:03:26 +0100

    Hangman - finished

--- Day7/Hangman.py
+++ Day7/Hangman.py
@@ -1,67 +1,51 @@
 import random
+from Hangman_art import stages, logo
+from Hanhman_words import word_list
 
-hangman_database = ['konstantynopolitanczykowianeczka']
-word = list(random.choice(hangman_database))
+word = list(random.choice(word_list))
 
 # declarations
-if_contains = []
-hangman = []
-user_indexes = []
-strikes = []
-bank = []
-repetition = False
-
+display = []
+lives = 6
+end_of_game = False
 
 # functions
-def checking_letter(answer, indexes):
+def checking_letter(guess):
     for index, letter in enumerate(word):
-        if letter == answer:
-            indexes.append(index)
-            if_contains.append(letter)
+        if letter == guess:
+            display[index] = guess
 
 
-def initializing_list():
-    for i in range(len(word)):
-        hangman.append('-')
-    print(''.join(hangman))
+def initializing():
+    for i in word:
+        display.append('-')
+    print(''.join(display))
+    print(logo)
 
 
 # starting program
-initializing_list()
+initializing()
 
-while True:
-    user_try = input('Insert a letter: ')
-    user_try = user_try.lower()
-    checking_letter(user_try, user_indexes)
+while not end_of_game:
+    user_try = input('Insert a letter: ').lower()
+
+    if user_try in display:
+        print(f"You've already guessed {user_try} letter before")
+    checking_letter(user_try)
+
+    print(''.join(display))  # printing hangman as string
 
     # checking if word contains this letter
-    if len(if_contains) == 0:
-        strikes.append('1')
-        print(f"You've made {len(strikes)} mistake(s)")
-
-    # checking if user typed this letter before
-    for tri in bank:
-        if tri == user_try:
-            repetition = True
-
-    # condition statement if try another one, or run hangmaan
-    if repetition:
-        print("You've already typed this letter. Try another one")
-    else:
-        bank.append(user_try)  # appending to list 'bank' - helps with repetition check. Bank for all typed letters.
-        for iterate_indexes in user_indexes:  # writting a letters at proper indexes
-            hangman[int(iterate_indexes)] = user_try
-
-        print(''.join(hangman))  # printing hangman as string
-
-        if len(strikes) == 7:  # if player has made 7 mistakes, hangman is hang
+    if user_try not in word:
+        lives -= 1
+        print(f"You guessed {user_try}, that's not in the word. You lose a life.")
+        if lives == 0:  # if player has made 7 mistakes, hangman is hang
             print('Unfortunately you lost :(')
-            break
-        if hangman == word:  # player won
-            print('Congrats! You won the game :)')
-            break
+            end_of_game = True
 
-    # clearing used tables and booleans
-    user_indexes = []
-    if_contains = []
-    repetition = False
+    if display == word:  # player won
+        print('Congrats! You won the game :)')
+        end_of_game = True
+
+    print(f"{stages[lives]}")
+
